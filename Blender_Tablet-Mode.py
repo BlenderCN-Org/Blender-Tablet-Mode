c59ddee3b05d3b26f5c21fd00c011b0a38381d2a
voyager25 <matthiasellerbeck@web.de>
2015-10-01 19:11:57 +0200

    Added button section, currently not showing up in the UI, added GPL license block.

--- Blender_Tablet-Mode.py
+++ Blender_Tablet-Mode.py
@@ -1,9 +1,27 @@
+### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
 bl_info = {
     "name": "Tablet Mode",
     "author": "Matthias Ellerbeck",
-    "version": (0, 0, 1),
+    "version": (0, 0, 2),
     "blender": (2, 76, 0),
-    "location": "No Location",
+    "location": "Node Editor",
     "description": "Enables a Tablet Mode for Blender",
     "warning": "Early development state!",
     "wiki_url": "",
@@ -12,32 +30,43 @@ bl_info = {
     
 import bpy
 
+#main operator
 class MouseClickModalOperator(bpy.types.Operator):
-    bl_idname = "tabletmode.activate"
+    bl_idname = "wm.tabletmode"
     bl_label = "Activate Tablet Mode"
 
     def modal(self, context, event):
-        if event.is_tablet == False:
+        if event.is_tablet != True: #Ignoring all non-tablet events
             return{'PASS_THROUGH'}
-        print(context)
+        if context.area.type == 'NODE_EDITOR': #Does not work, gives back 'USER_PREFERENCES' every time
+            print("Yay!")
+            return{'PASS_THROUGH'}
         return{'RUNNING_MODAL'}
     
     def invoke(self, context, event):
         context.window_manager.modal_handler_add(self)
         return {'RUNNING_MODAL'}
 
-def register():
-    bpy.utils.register_class(MouseClickModalOperator)
-    #bpy.utils.register_manual_map(add_object_manual_map)
-    #bpy.types.INFO_MT_mesh_add.append(add_object_button)
+#adding buttons for common used functions
+def NodeEditorPanel(self,context):
+    layout = self.layout
+    col = layout.column()
+    row = col.row(align=True)
 
+    row.alignment = 'CENTER'
+    row.operator("bpy.ops.node.delete()", icon="X", text="Delete") #Delete button, currently doesn't work
+
+#registration
+def register():
+    #bpy.utils.register_module(__name__)
+    bpy.utils.register_class(MouseClickModalOperator)
+    bpy.types.NODE_HT_header.append(NodeEditorPanel)
 
 def unregister():
+    #bpy.utils.unregister_module(__name__)
     bpy.utils.unregister_class(MouseClickModalOperator)
-    #bpy.utils.unregister_manual_map(add_object_manual_map)
-    #bpy.types.INFO_MT_mesh_add.remove(add_object_button)
-
+    bpy.types.NODE_HT_header.remove(NodeEditorPanel)
 
 if __name__ == "__main__":
     register()
-    bpy.ops.tabletmode.activate()
+    #bpy.ops.wm.tabletmode('INVOKE_SCREEN')
